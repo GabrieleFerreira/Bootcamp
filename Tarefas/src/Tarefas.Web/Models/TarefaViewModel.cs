d212a1bf08b83a3e73486293c8d7f1a7772aa6b8
GabrieleFerreira <gabriele.ferreira@ufvjm.edu.br>
2023-01-24 21:29:07 -0300

    Iniciando o formulario

---
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -0,0 +1,10 @@
+namespace Tarefas.Web.Controllers
+{
+    public class TarefaViewModel{
+     
+            public string Titulo {get;set;}
+            public string Descricao {get;set;}
+
+        
+    }
+}
